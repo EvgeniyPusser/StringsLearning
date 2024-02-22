72fab50bc070f8e25357d6f3bfc442e711616045
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-23 00:52:25 +0300

    My strings my love

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// string? gaga = "Gegoal";
+// string? vowels = "aeuioy";
+// foreach(char t in vowels){
+// if(gaga.Contains(t)){
+//     Console.Write("yes\t");
+// }}
+
+string news = " To&get&to know smth you need to come";
+var array = news.Split("&");
+// foreach(string y in array)
+// Console.Write(y + "\t");
+// Console.Write(string.Join("**", array));
+Console.Write(news.IndexOf("smth"));
+Console.Write(news[7]);
+Console.Write(array.GetType());
