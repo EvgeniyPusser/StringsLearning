6ad1c26f59cb03befadc39796da4dc17ddd44e02
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-24 03:00:25 +0300

    NewTask1

---
+++ newTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+char[,] arrayChar =  {
+  {'t', '@', 'p'},
+  {'h', '^', 'k'},
+  {'q', 'f','z'}
+  };
+
+
+   string NewOne = "";
+  foreach(char t in arrayChar){
+  NewOne += t + " ";}
+
+ 
+
+Console.Write(NewOne);
+
+//Console.Write(string.Join("   ",arrayChar));
+
