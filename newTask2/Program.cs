6e253afa3cc09b6aa161e4a892d9ff7ca877320a
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-24 03:02:09 +0300

    Task2

---
+++ newTask2/Program.cs
@@ -0,0 +1,27 @@
+﻿// string LatinUpper = "New things Get Better";
+// Console.Write(LatinUpper.ToLower());
+
+string IsPalindrom = "ootreatiaertoo";
+bool verify = true;
+for( int i = 0; i < IsPalindrom.Length / 2; i++)
+{
+    if(IsPalindrom[i]== IsPalindrom[IsPalindrom.Length - i -1])
+    {
+        verify = verify && true;
+       
+    }
+    else
+    {
+         verify = verify && false;   
+    }
+
+        
+} 
+    if(verify)
+{Console.Write("It's palindrom");
+}
+else
+{
+    Console.Write("No");}
+
+
