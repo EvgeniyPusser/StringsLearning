7dbb39018b257d746a8b361fd3dd5d037832bb7d
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-24 03:04:09 +0300

    Task3

---
+++ newTask3/Program.cs
@@ -0,0 +1,8 @@
+﻿string ForTransform = "Hi my heart baby I love you";
+string[] array = ForTransform.Split(" ");
+string [] newArray = new string[array.Length];
+for(int i =0; i < array.Length; i++){
+    newArray[i] = array[array.Length -i -1];
+}
+Console.Write(string.Join(" ", newArray));
+Console.Write("\n");
