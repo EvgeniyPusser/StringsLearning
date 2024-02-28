19ca26d71de73a068650973bc5db49ec392ab440
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-28 22:21:11 +0300

    neaTask3

--- newTask3/Program.cs
+++ newTask3/Program.cs
@@ -1,8 +1,23 @@
-﻿string ForTransform = "Hi my heart baby I love you";
+﻿void Main()
+{
+    string ForTransform = "Hi my heart baby I love you";
 string[] array = ForTransform.Split(" ");
+for(int i = 1; i < 3; i++){
+   
+
+    Console.Write($"{array[i]} _-");
+}
+Console.Write("   ");
 string [] newArray = new string[array.Length];
 for(int i =0; i < array.Length; i++){
     newArray[i] = array[array.Length -i -1];
 }
-Console.Write(string.Join(" ", newArray));
+Console.Write(string.Join("", newArray));
 Console.Write("\n");
+
+string Mama = string.Join("-MAMA-", array);
+Console.Write(Mama);
+}
+
+Main();
+
